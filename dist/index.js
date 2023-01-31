9fc4409a5eca85dd2402f4fef53b5f18b2e888b5
PrashantSL-IBO <prashant.laxmeshwarmath@ibo.com>
2023-01-31 12:14:43 +0530

    basic types1 of typerscript

--- dist/index.js
+++ dist/index.js
@@ -9,3 +9,38 @@ let x = true;
 x = 99;
 let marks = [97, 80, 97, 69, 90];
 marks.push(98);
+// Tuple
+let random = ["Prashant", 10, true];
+random.push(100);
+// Tuple array
+let dbTable = ["Prashant", 22, true];
+dbTable.push("haveMobile");
+let employees;
+employees = [
+    ["Prashant", 22],
+    ["Kausthubh", 25],
+    ["Siddharth", 32],
+];
+employees.push(["true", 100]);
+// Union
+let product_id = 22;
+product_id = "bro@gmail.com";
+const user = {
+    age: 22,
+    name: "Prashant",
+};
+// Type assertion
+const cid = 1;
+// let customerId=<number>cid
+let customerId = cid;
+// functions
+function addNum(x, y) {
+    return x + y;
+}
+addNum(20, 10);
+// void
+function logmsg(msg) {
+    console.log(msg);
+}
+logmsg("Good day");
+logmsg(100);
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,55 @@ x = 99;
 
 let marks: number[] = [97, 80, 97, 69, 90];
 marks.push(98);
+
+// Tuple
+let random: any[] = ["Prashant", 10, true];
+random.push(100);
+
+// Tuple array
+let dbTable: [string, number, boolean] = ["Prashant", 22, true];
+dbTable.push("haveMobile");
+
+let employees: [string, number][];
+employees = [
+  ["Prashant", 22],
+  ["Kausthubh", 25],
+  ["Siddharth", 32],
+];
+employees.push(["true", 100]);
+
+// Union
+let product_id: string | number = 22;
+product_id = "bro@gmail.com";
+
+// Object
+type User = {
+  age: number;
+  name: string;
+};
+
+const user: User = {
+  age: 22,
+  name: "Prashant",
+};
+
+// Type assertion
+const cid: any = 1;
+
+// let customerId=<number>cid
+let customerId = cid as number;
+
+// functions
+function addNum(x: number, y: number): number {
+  return x + y;
+}
+addNum(20, 10);
+
+// void
+function logmsg(msg: string | number): void {
+  console.log(msg);
+}
+logmsg("Good day");
+logmsg(100);
+
+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;
